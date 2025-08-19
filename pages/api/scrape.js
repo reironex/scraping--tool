2843a27cb5a3ded953e5fe84e5754f77675bbfda
reironex <nexreiro@gmail.com>
2025-08-19 10:09:42 +0800

    Update scrape.js

--- pages/api/scrape.js
+++ pages/api/scrape.js
@@ -2,18 +2,20 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 
 export default async function handler(req, res) {
+  const { url } = req.query;
+  if (!url) return res.status(400).json({ error: 'URL is required' });
+
   try {
-    const response = await axios.get('http://quotes.toscrape.com/');
+    const response = await axios.get(url);
     const $ = cheerio.load(response.data);
 
-    const quotes = [];
-    $('.quote').each((i, el) => {
-      const text = $(el).find('.text').text();
-      const author = $(el).find('.author').text();
-      quotes.push({ text, author });
+    // Example: kunin lahat ng <p> text sa page
+    const paragraphs = [];
+    $('p').each((i, el) => {
+      paragraphs.push($(el).text());
     });
 
-    res.status(200).json(quotes);
+    res.status(200).json({ paragraphs });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Error scraping site' });
